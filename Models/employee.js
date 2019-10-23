0e2cef0ddf4ab37279ece5d0126e4e713d938b24
Joshua <jdkelly2015@gmail.com>
2019-10-23 13:58:38 -0400

    server is up and running. MVC installed. mongo connected

---
+++ Models/employee.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  rank: { type: String, required: true }
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+module.exports = Employee;
