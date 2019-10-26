6f625ef0b5de5cf31c20d94add3f4b6e57dad2a4
Joshua <jdkelly2015@gmail.com>
2019-10-25 21:30:54 -0400

    dnd components saved. successful compile

--- client/src/pages/ScheduleMaker.jsx
+++ client/src/pages/ScheduleMaker.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
-import { Row, Container } from "../components/Grid";
-import Header from "../components/Header";
 import API from "../utils/API";
-import { List, ListItem } from "../components/List";
-import Table from "../components/Table";
-import Footer from "../components/Footer";
-import SubmitButton from "../components/SubmitButton";
-import { Link } from "react-router-dom";
+
 class ScheduleMaker extends Component {
   state = {
     employees: [],
@@ -28,46 +22,7 @@ class ScheduleMaker extends Component {
   };
 
   render() {
-    return (
-      <Container fluid>
-        <Header />
-        <Row>
-          <div className="col-2">
-            {/* Board 1  */}
-            <Container>
-              <div className="col">
-                {this.state.employees.length ? (
-                  <List>
-                    {this.state.employees.map(employee => (
-                      <ListItem key={employee._id}>
-                        <Link
-                          to={"/employees/" + employee._id}
-                          className="text-dark"
-                        >
-                          <strong>{employee.name}</strong>
-                        </Link>
-                      </ListItem>
-                    ))}
-                  </List>
-                ) : (
-                  <h3>No Results to Display</h3>
-                )}
-              </div>
-            </Container>
-          </div>
-          <div className="col">
-            <Container>
-              <Table />
-              <Row>
-                <SubmitButton />
-              </Row>
-            </Container>
-          </div>
-        </Row>
-
-        <Footer />
-      </Container>
-    );
+    return <h1>Starting to bring in outside draggable components</h1>;
   }
 }
 
