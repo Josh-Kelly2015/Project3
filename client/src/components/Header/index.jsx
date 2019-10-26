0b356e6159cd1e6834f7280d951a4397e2f30849
Joshua <jdkelly2015@gmail.com>
2019-10-26 06:25:55 -0400

    added submit button to front end. fixed some CSS

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="row bg-dark mb-3">
+    <div className="row bg-dark mb-4">
       <div className="col">
         <h1 className="text-center text-info">WeSchedule</h1>
       </div>
