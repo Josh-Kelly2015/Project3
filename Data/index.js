fcf55690d0ee95edde60f455ec2f96c3b3176d09
Joshua <jdkelly2015@gmail.com>
2019-11-05 13:04:06 -0500

    errors fixxed

--- Data/index.js
+++ Data/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   ProjectData: require("./ProjectData"),
   EmployeeData: require("./EmployeeData"),
-  WorkWeekData: require("./WorkWeekData"),
   WorkDayData: require("./WorkDayData")
 };
--- Routes/api/index.js
+++ Routes/api/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 const EmployeeRoutes = require("./Employee");
 const ProjectRoutes = require("./Project");
 const WorkDayRoutes = require("./WorkDay");
-const WorkWeekRoutes = require("./WorkWeek");
 router.use("/employee", EmployeeRoutes);
 router.use("/project", ProjectRoutes);
 router.use("/workday", WorkDayRoutes);
-router.use("/workweek", WorkWeekRoutes);
 
 module.exports = router;
