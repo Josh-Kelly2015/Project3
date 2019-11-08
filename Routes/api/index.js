5fff5a565cb34215e7ca226b6cedc671886fb10f
Joshua <jdkelly2015@gmail.com>
2019-11-07 20:56:05 -0500

    refixed routes

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const EmployeeRoutes = require("./Employee");
 const ProjectRoutes = require("./Project");
 // const WorkDayRoutes = require("./WorkDay");
-router.use("/employees", EmployeeRoutes);
-router.use("/projects", ProjectRoutes);
+router.use("/employee", EmployeeRoutes);
+router.use("/project", ProjectRoutes);
 // router.use("/workday", WorkDayRoutes);
 
 module.exports = router;
