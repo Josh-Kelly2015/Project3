ee89c8b9d38d6a2c039a84c85d0a647f11683ad8
Joshua <jdkelly2015@gmail.com>
2019-11-09 21:40:57 -0500

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-import Home from "./components/Home
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Navbar >
+          <Navbar>
             <Route exact path="/login" component={Login} />
             <Route exact path="/" component={Home} />
           </Navbar>
         </div>
       </Router>
-    )
+    );
   }
+}
 export default App;
