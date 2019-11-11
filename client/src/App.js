af338a8e8a2f65d324c0ffb2271fcb6425c9ea52
Joshua <jdkelly2015@gmail.com>
2019-11-11 03:54:14 -0500

    trying to get login page and home paage working

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Navbar>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/home" component={Home} />
-        </Navbar>
+        {/* <Navbar> */}
+        {/* <Route exact path="/login" component={Login} /> */}
+        <Route exact path="/" component={Home} />
+        {/* </Navbar> */}
       </Router>
     );
   }
