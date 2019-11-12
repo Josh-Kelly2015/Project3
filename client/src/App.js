e301cb737eefd7878927c29230ad611aa12cd2d5
Joshua <jdkelly2015@gmail.com>
2019-11-11 19:41:51 -0500

    we're almost there fixing the bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./components/Home";
 import Login from "./components/UserAuth/Login";
-import Profile from "./components/UserAuth/Profile";
+// import Profile from "./components/UserAuth/Profile";
 import Register from "./components/UserAuth/Register";
 function App() {
   return (
     <Router>
       <Route exact path="/main" component={Main} />
-      <Route exact path="/" component={Login} />
-      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/login" component={Login} />
+      {/* <Route exact path="/profile" component={Profile} /> */}
       <Route exact path="/register" component={Register} />
     </Router>
   );
--- client/src/components/UserAuth/UserFunctions.js
+++ client/src/components/UserAuth/UserFunctions.js
@@ -15,16 +15,28 @@ export const login = user => {
     });
 };
 
-// export const getProfile = user => {
-//   return axios
-//     .get("users/profile", {
-//       headers: { Authorization: ` ${this.getToken()}` }
-//     })
-//     .then(response => {
-//       console.log(response);
-//       return response.data;
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+export const getProfile = user => {
+  return axios
+    .get("users/profile", {
+      headers: { Authorization: ` ${this.getToken()}` }
+    })
+    .then(response => {
+      console.log(response);
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+export const register = newUser => {
+  return axios
+    .post("users/register", {
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      password: newUser.password
+    })
+    .then(response => {
+      console.log("Registered");
+    });
+};
